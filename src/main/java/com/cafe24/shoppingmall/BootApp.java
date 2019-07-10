3390b3498d403a4575f28cd9d3d167148813652a
rkskekabc <shinde13@naver.com>
2019-07-10 20:27:38 +0900

    first commit

---
+++ src/main/java/com/cafe24/shoppingmall/BootApp.java
@@ -0,0 +1,13 @@
+package com.cafe24.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cafe24.shoppingmall.BootApp;
+
+@SpringBootApplication
+public class BootApp {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApp.class, args);
+	}
+}
